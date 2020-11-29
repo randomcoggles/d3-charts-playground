dadde8ebae1fa378280f3fbc67565ddb65a44621
randomcoggles <randomcoggles@gmail.com>
2020-11-29 15:29:22 -0300

    Get caroussel data from mock instead of inline code

---
+++ src/app/components/three-d-carroussel/three-d-carroussel.models.ts
@@ -0,0 +1,7 @@
+export interface CarrousselCard {
+  title: string;
+  icon?: string;
+  iconUrl?: string;
+  catchyWords?: string[];
+  bfbgColor?: string;
+}
--- src/app/components/three-d-carroussel/three-d-carroussel.module.ts
+++ src/app/components/three-d-carroussel/three-d-carroussel.module.ts
@@ -4,11 +4,12 @@ import { ThreeDCarrousselComponent } from "./three-d-carroussel.component";
 import { CarrousselControllerComponent } from "./components/carroussel-controller/carroussel-controller.component";
 import { MaterialComponentsModule } from "../../shared/material-components/material-components.module";
 import { ThreeDCarrousselManager } from "./three-d-carroussel.manager";
+import { ThreeDCarrousselService } from "./three-d-carroussel.serivce";
 
 @NgModule({
   imports: [CommonModule, MaterialComponentsModule],
   declarations: [ThreeDCarrousselComponent, CarrousselControllerComponent],
   exports: [ThreeDCarrousselComponent],
-  providers: [ThreeDCarrousselManager]
+  providers: [ThreeDCarrousselService, ThreeDCarrousselManager]
 })
 export class ThreeDCarrousselModule {}
